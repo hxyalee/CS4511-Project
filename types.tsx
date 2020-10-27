95ba574abd74bd3ca3cb19a9dd108a8fa7a871f6
Hoya Lee <z5226463@ad.unsw.edu.au>
2020-10-27 13:58:58 +1100

    reset

---
+++ types.tsx
@@ -0,0 +1,20 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Search: undefined;
+  Add: undefined;
+  Notifications: undefined;
+  Profile: undefined;
+};
+
+export type TabOneParamList = {
+  Home: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
