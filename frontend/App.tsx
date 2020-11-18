9fcee1ebbc209a5ab16649a2224d1891728fe457
allimurray <allizabethm@gmail.com>
2020-11-18 15:22:06 +1100

    Fixed async deprecation issues

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
